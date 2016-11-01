9c247a4cb20676f3bbbf42f92ff470ae3cf00e92
Drkpkg <daniel.uremix@gmail.com>
2016-11-01 13:09:05 -0400

    Cambios en Punto fijo.

--- fixed_point.py
+++ fixed_point.py
@@ -6,11 +6,11 @@ class FixedPoint(Equation):
         error = 100
         while error > error_limit:
             next_value = float(self.solve(x))
-            error = self.get_percentual_error(x, next_value)
             self.sections.append({'x': next_value, 'error': error})
+            error = self.get_percentual_error(x, next_value)
             x = next_value
 
 
 # fpoint = FixedPoint("E**-x", 'x')
-# fpoint.solve_fixed(1, 0.1)
+# fpoint.solve_fixed(1, 0.01)
 # print(fpoint.sections)
