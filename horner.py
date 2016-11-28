69d38515dfd7e8e6b5f90870ce5d75dce7f9cc88
Drkpkg <daniel.uremix@gmail.com>
2016-11-27 21:58:11 -0400

    Cambios en horner

--- horner.py
+++ horner.py
@@ -1,6 +1,5 @@
-from sympy import poly, degree, symbols
+from sympy import poly, degree, symbols, Symbol, Poly
 from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application, parse_expr
-
 
 class Horner:
     def __init__(self, polynomial, polynomial_divider, variable):
@@ -13,6 +12,8 @@ class Horner:
             self.polynomial = poly(self.dividend)
             self.polynomial_divider = poly(self.divider)
 
+            self.quotient = None
+            self.residue = None
             self.sections = []
         except:
             return "Expression cannot be formatted"
@@ -24,9 +25,10 @@ class Horner:
         dividend = self.polynomial.all_coeffs()
         divider = self.inverted_poly()
         position_dividend = 1
+        distance = len(dividend)-(len(divider)-1)
         i = 0
 
-        while i < (len(dividend)-(len(divider)-1)):
+        while i < distance:
             d = dividend[i] / divider[0]
             self.sections.append(d)
 
@@ -37,11 +39,17 @@ class Horner:
                 position_dividend += 1
             position_dividend -= 1
             i += 1
-        return dividend
+
+        self.quotient = self.parse_poly(dividend[0:distance])
+        self.residue = self.parse_poly(dividend[distance:len(dividend)])
 
     @staticmethod
-    def inverted_poly():
-        h = horner.polynomial_divider.all_coeffs()
+    def parse_poly(data):
+        x = Symbol('x')
+        return Poly.from_list(data, gens=x)
+
+    def inverted_poly(self):
+        h = self.polynomial_divider.all_coeffs()
         new = [h[0]]
         counter = 1
         while counter < len(h):
@@ -50,8 +58,9 @@ class Horner:
         return new
 
 
-horner = Horner("38x**4-65x**3+0x**2+0x+27", "2x**2-5x+3", 'x')
-print(horner.solve_horner())
+# horner = Horner("38x**4-65x**3+0x**2+0x+27", "2x**2-5x+3", 'x')
+# print(horner.solve_horner())
 
 horner = Horner("8x**5+14x**4+5x**3+16x**2+3x+2", "4x**2+x+3", 'x')
-print(horner.solve_horner())
+horner.solve_horner()
+print(horner.quotient, horner.residue)
