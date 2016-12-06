2b367b0996e5a0502ef7c4afcf90348ff73d64ff
Drkpkg <daniel.uremix@gmail.com>
2016-12-05 22:48:37 -0400

    Metodo de Gauss terminado

--- gauss.py
+++ gauss.py
@@ -18,6 +18,11 @@ class Gauss:
                 self.equations[i] = poly(self.equations[i])
                 self.eq_coeffs.append(self.equations[i].all_coeffs())
 
+            r = 0
+            for k in self.eq_coeffs:
+                k[len(k)-1] = int(self.terms[r])
+                r += 1
+
         except TypeError:
             print("Expression cannot be formatted")
 
@@ -29,28 +34,54 @@ class Gauss:
             terms.append(actual[1])
 
     def solve_gauss(self):
-        while not self.is_scaled_matrix():
-            pass
+        n = len(self.eq_coeffs)
+        for i in range(0, n):
+            # Search for maximum in this column
+            max_el = abs(self.eq_coeffs[i][i])
+            max_row = i
+            for k in range(i + 1, n):
+                if abs(self.eq_coeffs[k][i]) > max_el:
+                    max_el = abs(self.eq_coeffs[k][i])
+                    max_row = k
 
-    def is_scaled_matrix(self):
-        i = 1
-        is_scaled = False
-        for row in self.eq_coeffs:
-            for position in range(len(row) - len(row) + i):
-                if row[position] == 0:
-                    is_scaled = True
-                else:
-                    is_scaled = False
-            i += 1
-        return is_scaled
+            # Swap maximum row with current row (column by column)
+            for k in range(i, n + 1):
+                tmp = self.eq_coeffs[max_row][k]
+                self.eq_coeffs[max_row][k] = self.eq_coeffs[i][k]
+                self.eq_coeffs[i][k] = tmp
 
-    @staticmethod
-    def switch(a, b):
-        c = a
-        a = b
-        b = c
+            # Make all rows below this one 0 in current column
+            for k in range(i + 1, n):
+                c = -self.eq_coeffs[k][i] / self.eq_coeffs[i][i]
+                for j in range(i, n + 1):
+                    if i == j:
+                        self.eq_coeffs[k][j] = 0
+                    else:
+                        self.eq_coeffs[k][j] += c * self.eq_coeffs[i][j]
 
-if __name__ == '__main__':
-    gauss = Gauss("2x**3 + 18x**2 + 4x = 1", "x**3 + 5x**2 + x = 10", "5x**3 + 4x**2 + 9x = 9", var='x')
-    print(gauss.eq_coeffs)
-    print(gauss.is_scaled_matrix())
+        # Solve equation Ax=b for an upper triangular matrix A
+        x = [0 for i in range(n)]
+        for i in range(n - 1, -1, -1):
+            x[i] = self.eq_coeffs[i][n] / self.eq_coeffs[i][i]
+            for k in range(i - 1, -1, -1):
+                self.eq_coeffs[k][n] -= self.eq_coeffs[k][i] * x[i]
+        self.sections = x
+
+    # def is_scaled_matrix(self):
+    #     i = 1
+    #     is_scaled = False
+    #     for row in self.eq_coeffs:
+    #         for position in range(len(row) - len(row) + i):
+    #             if row[position] == 0:
+    #                 is_scaled = True
+    #             else:
+    #                 is_scaled = False
+    #         i += 1
+    #     return is_scaled
+
+# if __name__ == '__main__':
+#     gauss = Gauss("2x**3+18x**2+4x=1", "x**3+5x**2+x=10", "5x**3+4x**2+9x=9", var='x')
+#     gauss.solve_gauss()
+#     print(gauss.is_scaled_matrix())
+#     print(gauss.sections)
+#     print(gauss.eq_coeffs)
