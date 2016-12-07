5c7468868b27704580ccad94993db880792b911c
Drkpkg <daniel.uremix@gmail.com>
2016-12-07 09:51:21 -0400

    Se quita ejemplo de prueba en simpson.

--- simpson.py
+++ simpson.py
@@ -22,8 +22,8 @@ class Simpson(Equation):
         result = ((3/8)*h)*(fa + (3 * fab) + fb)
         return float(result)
 
-if __name__ == '__main__':
-    # simpson = Simpson("(2x+3)/(x**2+3x+2)", 'x')
-    simpson = Simpson("(x(E**(3x)))", 'x')
-    print(simpson.solve_with_simple_simpson(0, 1))
-    print(simpson.solve_with_3_8_simpson(0, 1, 3))
+# if __name__ == '__main__':
+#     # simpson = Simpson("(2x+3)/(x**2+3x+2)", 'x')
+#     simpson = Simpson("(x(E**(3x)))", 'x')
+#     print(simpson.solve_with_simple_simpson(0, 1))
+#     print(simpson.solve_with_3_8_simpson(0, 1, 3))
